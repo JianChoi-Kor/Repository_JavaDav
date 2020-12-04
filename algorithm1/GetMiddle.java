b605e11e90da630588836a1352a5f960a22d364e
JianChoi-Kor <wldks79@gmail.com>
2020-12-04 21:06:54 +0900

    20201204

--- algorithm1/GetMiddle.java
+++ algorithm1/GetMiddle.java
@@ -1,12 +1,16 @@
 package algorithm1;
 
-
-public class Test {
+public class GetMiddle {
 
 	public static void main(String[] args) {
 		
-		Test gm = new Test();
+		GetMiddle gm = new GetMiddle();
 		System.out.println(gm.getMiddle("ABCDEFGHIJ"));
+		
+		
+		String str = "abcde";
+		
+		System.out.println(str.substring(0, 2));
 	}
 	
 	public String getMiddle(String word) {
@@ -15,5 +19,6 @@ public class Test {
 		
 	}
 }
+
 
 
